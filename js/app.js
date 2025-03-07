2ab0092d4652ac0ab0a7e73649f7969ee08b4705
allansgsdev <allanscgomes.dev@gmail.com>
2025-03-07 18:06:06 -0300

    Upload de arquivos (no program)

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// *PÁGINA DE DESAFIOS*
+
+// Desafio: Sorteio - Amigo Secreto
+// Autor: Allan Gomes
+// Curso: Lógica de programação - Praticando com desafios
+// Instituição: Alura
+
+
+// DECLARAÇÃO E ATRIBUÇÃO DE VARIÁVEIS
+
+
+// DECLARAÇÃO DE FUNÇÕES
+
+
+// PROGRAMA PRINCIPAL
